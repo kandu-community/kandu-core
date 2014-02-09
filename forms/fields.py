79b879782b84505f90f1d490dfb34e29ccfa73a9
colorfulfool <colorfulfool@gmail.com>
2014-02-09 15:44:42 +0300

    README updated

--- forms/fields.py
+++ forms/fields.py
@@ -1,4 +1,7 @@
 from django.db.models import CharField
 
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^forms\.fields\.CoordinatesField"])
+
 class CoordinatesField(CharField):
 	pass

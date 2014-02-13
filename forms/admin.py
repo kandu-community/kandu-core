bd2ed262935b1d925fd12d19124338dd40178c26
colorfulfool <colorfulfool@gmail.com>
2014-02-13 20:47:41 +0300

    "fields_for_label" now also used in admin

--- forms/admin.py
+++ forms/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from utils import get_form_models
 
 for form_name, form_model in get_form_models():
-	admin.site.register(form_model)
+	try:
+		class FormAdmin(admin.ModelAdmin):
+			list_display = form_model.label_fields
+
+		admin.site.register(form_model, FormAdmin)
+		
+	except AttributeError:
+		admin.site.register(form_model)

b28ddbd66a68b5c79d8714a84c0d5d61451de89f
colorfulfool <colorfulfool@gmail.com>
2014-02-25 16:13:39 +0300

    URL names changed

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.FormList.as_view(), name='api_list'),
-    url(r'^(?P<model_name>\w+)/$', views.FormList.as_view(), name='api_list'),
-    url(r'^(?P<model_name>\w+)/(?P<pk>\d+)/$', views.FormDetail.as_view(), name='api_detail'),
+    url(r'^getSubmissions/$', views.BaseFormList.as_view(), name='api_list'),
+    url(r'^getSubmissions/(?P<model_name>\w+)/$', views.FormList.as_view(), name='api_list'),
+    url(r'^getSubmissions/(?P<model_name>\w+)/(?P<pk>\d+)/$', views.FormDetail.as_view(), name='api_detail'),
 
     url(r'^get-token/$', 'rest_framework.authtoken.views.obtain_auth_token'),
     url(r'^browseable-auth/$', include('rest_framework.urls', namespace='rest_framework')) #for the browseable API

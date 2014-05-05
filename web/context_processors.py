8d1a29b765f1eef1071fe3cde086766fb89c0f33
colorfulfool <colorfulfool@gmail.com>
2014-05-05 16:35:34 +0300

    regroup instead of groupby

--- web/context_processors.py
+++ web/context_processors.py
@@ -3,4 +3,4 @@ from itertools import groupby
 
 def form_models(request):
 	# return { 'form_models': [ (category, list(models)) for category, models in groupby( get_form_models(for_user=request.user), key=lambda (name,model): model.category ) ] }
-	return { 'form_models': groupby( get_form_models(for_user=request.user), key=lambda (name,model): model.category ) }
+	return { 'form_models': [form_model for form_name, form_model in get_form_models(for_user=request.user)] }

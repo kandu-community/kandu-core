09e96de64921b99958f6ceff1425e1e570ef9ee8
colorfulfool <colorfulfool@gmail.com>
2014-05-28 00:37:03 +0300

    Empty forms.models to prevent errors on initial deployment

--- kandu/local_settings.py
+++ kandu/local_settings.py
@@ -7,7 +7,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'kandu',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'postgres',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
     }
 }
 

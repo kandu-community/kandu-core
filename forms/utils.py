5c831740958e4f5f4736b3d12a965b15bb48bad0
colorfulfool <colorfulfool@gmail.com>
2014-06-29 20:46:29 +0300

    Fix of indetnation

--- forms/utils.py
+++ forms/utils.py
@@ -34,11 +34,11 @@ def clear_app_cache():
 	curdir = os.getcwd()
 
 	for app in cache.get_apps():
-	    f = app.__file__
-	    if f.startswith(curdir) and f.endswith('.pyc'):
-	        os.remove(f)
-	    __import__(app.__name__)
-	    reload(app)
+		f = app.__file__
+		if f.startswith(curdir) and f.endswith('.pyc'):
+			os.remove(f)
+		__import__(app.__name__)
+		reload(app)
 
 	# from django.utils.datastructures import SortedDict
 	# cache.app_store = SortedDict()

901deea7b7767dcbbfa024fea4dfe4c1e1aa7b4d
colorfulfool <colorfulfool@gmail.com>
2014-09-19 19:17:39 +0300

    visible_when now works for inline forms as well

--- web/templatetags/custom_tags.py
+++ web/templatetags/custom_tags.py
@@ -8,13 +8,13 @@ from web.widgets import SearchForm
 register = template.Library()
 
 @register.inclusion_tag('web/visibility_dependencies_tag.html')
-def visibility_dependencies_js(dependencies_dict):
+def visibility_dependencies_js(dependencies_dict, inline_prefix=None):
 	transponed_dict = defaultdict(lambda: defaultdict(list))
 	for slave_input, terms in dependencies_dict.items():
 		for master_input, value in terms.items():
 			transponed_dict[master_input][value].append(slave_input)
 
-	return { 'dependencies_dict': mark_safe(json.dumps(transponed_dict)) }
+	return { 'dependencies_dict': mark_safe(json.dumps(transponed_dict)), 'inline_prefix': inline_prefix }
 
 @register.inclusion_tag('web/search_form_tag.html')
 def search_form():
